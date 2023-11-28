2caea6f6822da411178aae9f2de0d9c8b942f7f5
Petrak-78 <129865911+Petrak-78@users.noreply.github.com>
2023-11-28 19:52:28 -0300

    Add files via upload

---
+++ Api de roupas/Objeto/input.js
@@ -0,0 +1,370 @@
+const items = [
+    {
+      title: 'Calça Cargo branca',
+      image: 'image 199.png',
+      estilo: 'streetWear',
+      id: 1,
+
+    },
+    {
+      title: 'Moletom Preto',
+      image: 'image 218.png',
+      estilo: 'streetWear',
+      id: 2,
+    },
+    {
+        title: 'Camisa streetwear lisa',
+        image: 'Rectangle 43.png',
+        estilo:'streetWear',
+        id: 3,
+      },
+      {
+        title: 'Camisa Oversized',
+        image: 'image 200.png',
+        estilo:'streetWear',
+        id: 4,
+      },
+      {
+        title: 'Camis Streetwear lisa',
+        image: 'image 203.png',
+        estilo: 'streetWear',
+        id: 5,
+      },
+            {
+        title: 'Streetwear simple',
+        image: 'image 96.png',
+        estilo:'streetWear',
+        id: 6,
+      },
+
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image:'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      {
+        title: 'Banana',
+        image: 'caminho/para/a/imagem/banana.jpg',
+        description: 'Uma banana madura e deliciosa.'
+      },
+      
+
+    // ... outros objetos
+
+
+  ];
+  
+  ////////////// Objetos acima
+
+
+
+  /////////////// aqui embaixo as funções começam 
+
+  function filterByCategory(category, items) {
+    category = category.toLowerCase();
+    
+    const filteredItems = items.filter(item => {
+      const itemCategory = item.category.toLowerCase();
+      return itemCategory === category;
+    });
+  
+    const productsContainer = document.getElementById('products-container');
+    productsContainer.innerHTML = '';
+  
+    filteredItems.forEach(item => {
+      const productDiv = document.createElement('div');
+      const productName = document.createElement('h3');
+      const productImage = document.createElement('img');
+      const productDescription = document.createElement('p');
+  
+      productName.textContent = item.name;
+      productImage.src = `caminho/para/as/imagens/${item.category}/${item.id}.jpg`;
+      productDescription.textContent = `Descrição: Alguma descrição do ${item.name}.`;
+  
+      productDiv.classList.add('headproduct');
+      productName.classList.add('vlrfnl');
+      productImage.classList.add('img-box');
+      productDescription.classList.add('vlrinc');
+  
+      productDiv.appendChild(productName);
+      productDiv.appendChild(productImage);
+      productDiv.appendChild(productDescription);
+  
+      productsContainer.appendChild(productDiv);
+    });
+  }
+
+  
+function setupClickListeners() {
+  document.getElementById('streetwearBtn').addEventListener('click', () => filterByCategory('streetwear',items));
+  document.getElementById('casualBtn').addEventListener('click', () => filterByCategory('camisas casual', items));
+  document.getElementById('socialBtn').addEventListener('click', () => filterByCategory('camisas social',items));
+  document.getElementById('bonesBtn').addEventListener('click', () => filterByCategory('bonés', items));
+  document.getElementById('tenisBtn').addEventListener('click', () => filterByCategory('tênis', items));
+  document.getElementById('correntesBtn').addEventListener('click', () => filterByCategory('correntes', items));
+  document.getElementById('relogiosBtn').addEventListener('click', () => filterByCategory('relógios', items));
+}
+
+// Chamada para configurar os listeners de clique
+setupClickListeners();
+
---
+++ Api de roupas/Objeto/readme.md
@@ -0,0 +1,58 @@
+
+Api criada pela P-P Enterprises para tranalho de escola, caso queira utilizar tente entrar em contato
+
+Api de produtos para site de roupas
+
+12  camisas streetwear
+6  camisas casual
+6  camisas social
+12  Bonés 
+8  Tênis
+4  correntes
+8  relógios
+
+
+
+
+Streetwear
+
+inicio linha - 3
+têrmino linha - 111
+
+
+
+Social
+
+início linha - 115
+têrmino linha - 165
+
+
+
+Casual 
+
+início linha - 169
+têrmino linha - 219
+
+
+Boné 
+
+início linha - 223
+têrmino linha - 291
+
+
+Tênis 
+
+início linha - 294
+Têrmino linha - 363
+
+
+Corrente 
+
+início linha - 367
+Têrmino linha - 402
+
+
+Relôgio 
+
+início linha - 404
+têrmino linha - 473
